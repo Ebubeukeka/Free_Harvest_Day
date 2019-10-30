2a9a24d8e7799ba20c3919fe099f2d04d318690a
qthornton9 <qthornton9@yahoo.com>
2019-10-30 10:28:48 -0500

    added needed app info to project settings and urls

--- free_harvest_day_project/settings.py
+++ free_harvest_day_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.indiv_garden_info',
     'apps.free_harvest_day_app',
     'django.contrib.admin',
     'django.contrib.auth',
--- free_harvest_day_project/urls.py
+++ free_harvest_day_project/urls.py
@@ -2,5 +2,7 @@
 from django.conf.urls import url, include
 
 urlpatterns = [
+    
+    url(r'^', include('apps.indiv_garden_info.urls')),
     url(r'^', include('apps.free_harvest_day_app.urls')), 
 ]
